cda27212890786df65a7fdbbdd039623a4830b6c
zain <you@example.com>
2018-03-04 02:08:52 +0500

    index fix

--- nodcms/controllers/Appointment_admin.php
+++ nodcms/controllers/Appointment_admin.php
@@ -150,8 +150,6 @@ class Appointment_admin extends NodCMS_Controller
     // Admin Homepage (Choose a provider)
     function index($id=NULL)
     {
-		echo "worke";
-		die();
         if($id!=NULL){
             if($this->session->userdata('group_id')!=1 && $this->session->userdata('group_id')!=100){
                 $where = NULL;
