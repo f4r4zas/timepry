cc2bf51b829de2b1306f73b372315296b47ba629
Faraz Ahmed <f4r4z.a@gmail.com>
2018-03-04 12:52:46 +0500

    about and faq page

---
+++ nodcms/views/about.php
@@ -0,0 +1,82 @@
+<?php //include('header.php'); ?>
+
+<section class="inner-page-banner">
+    <div class="banner_wrapper">
+        <div class="banner_content">
+            <h1>About Us</h1>
+        </div>
+    </div>
+</section>
+
+<section class="inner-page-body">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-6">
+                <div class="section-heading">
+                    <h1>What We Do?</h1>
+                    <div class="cus-hr"></div>
+                </div>
+
+                <div class="section-content">
+                    <p>
+                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                    </p>
+                    <p class="blue_font">
+                        Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-6">
+                <div class="about_img">
+                    <img src="<?php echo base_url()?>assets/reservation/img/aboutus_img_thumb.jpg" alt="Timepry">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="aboutus-mission">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 box">
+                <div class="box-img">
+                    <img src="<?php echo base_url()?>assets/reservation/img/mission_icon.png" alt="Mission">
+                </div>
+                <div class="box-title">Mission</div>
+                <div class="box-content">
+                    <p>
+                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-md-4 box">
+                <div class="box-img">
+                    <img src="<?php echo base_url()?>assets/reservation/img/vision_icon.png" alt="Vision">
+                </div>
+                <div class="box-title">Vision</div>
+                <div class="box-content">
+                    <p>
+                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-md-4 box">
+                <div class="box-img">
+                    <img src="<?php echo base_url()?>assets/reservation/img/objective_icon.png" alt="Objective">
+                </div>
+                <div class="box-title">Objective</div>
+                <div class="box-content">
+                    <p>
+                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<?php //include('footer.php'); ?>
